1d181eed377032d38279e6abf7150c13d368a04a
Admin <baisanehira@gmail.com>
2024-08-07 23:43:36 +0530

    8/7/2024

--- lib/Provider/quotes_provider.dart
+++ lib/Provider/quotes_provider.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import '../Api_calling/quotes.dart';
 import '../Modal/modal.dart';
-import '../Screens/HomePage/HomePage.dart';
 
 class QuotesProvider extends ChangeNotifier {
   Api_Calling api_calling = Api_Calling();
